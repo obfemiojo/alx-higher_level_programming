6016e77c703e4ba863c1c47772130d0581357a51
root <obafemiojo97@gmail.com>
2022-06-02 06:45:35 -0700

    Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+from add_0 import add
+
+
+def main():
+    a = 1
+    b = 2
+    c = add(a, b)
+    print("{} + {} = {}".format(a, b, c))
 
 if __name__ == "__main__":
-    import calculator_1
-    a = 10
-    b = 5
-
-    print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
-    print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
-    print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
-    print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+    main()
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main__":
-    import calculator_1
+
+def main():
     a = 10
     b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
 
-    print("{} + {} = {}".format(a, b, calculator_1.add(a, b)))
-    print("{} - {} = {}".format(a, b, calculator_1.sub(a, b)))
-    print("{} * {} = {}".format(a, b, calculator_1.mul(a, b)))
-    print("{} / {} = {}".format(a, b, calculator_1.div(a, b)))
+if __name__ == "__main__":
+    main()
