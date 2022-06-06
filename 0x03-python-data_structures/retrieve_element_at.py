7e210e6464c84a85bc9736069277fd64ac0498a5
root <obafemiojo97@gmail.com>
2022-06-06 15:17:44 -0700

    Switch

---
+++ 0x03-python-data_structures/retrieve_element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# retrieve_element_at.py
+# Ojo Obafemi David
+
+def element_at(my_list, idx):
+    """Retrieve an element from a list."""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
