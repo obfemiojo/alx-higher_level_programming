0571e694aee057094a04467454c6ab767f81246c
root <obafemiojo97@gmail.com>
2022-07-15 04:04:35 -0700

    Make Tea not Love

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,5 +2,5 @@
 # 2-print_alphabet.py
 
 """Print the alphabet in lowercase, not followed by a new line."""
-for i in range(97, 123):
-    print(chr(i), end="")
+for letter in range(97, 123):
+    print('{}'.format(chr(letter)), end="")
